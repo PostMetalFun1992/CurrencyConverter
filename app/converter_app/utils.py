e0c4b70ab6bb3e8873773447c52947be73b35d77
PostMetalFun1992 <kabanovkirill54@gmail.com>
2018-10-08 21:52:22 +0700

    Currency rate request

--- app/converter_app/utils.py
+++ app/converter_app/utils.py
@@ -1,15 +1,23 @@
 import os
 
 import requests as r
+from rest_framework import status
+
 
 OEX_RATES_URL = 'https://openexchangerates.org/api/latest.json'
 OEX_APP_ID = os.getenv('OEX_APP_ID', '')
+CURRENCIES = os.getenv('CURRENCIES', '').split(',')  # TODO: prevent typos
 
 
-def get_currency_rate():
-    response = r.get(
+def get_currency_rate(base_currency, converted_currencies):
+    resp = r.get(
         OEX_RATES_URL,
-        params={'app_id': OEX_APP_ID}
+        params={'app_id': OEX_APP_ID, 'base': base_currency}
     )
 
-    print(response.json())
+    if not resp.status_code == status.HTTP_200_OK:
+        return {}
+
+    rates = resp.json()['rates']
+
+    return {curr: rates[curr] for curr in converted_currencies}
