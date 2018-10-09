9293c3623c662c9fb2e939a4b86d1d4c29816390
PostMetalFun1992 <kabanovkirill54@gmail.com>
2018-10-09 23:16:34 +0700

    Currency rate model

--- app/converter_app/models.py
+++ app/converter_app/models.py
@@ -1,1 +1,32 @@
+import os
+from datetime import datetime
+
+from django.core.exceptions import ValidationError
 from django.db import models
+
+
+RATES_UPDATE_PERIOD_DAYS = int(os.getenv('RATES_UPDATE_PERIOD_DAYS', '1'))
+
+
+class CurrencyRate(models.Model):
+    base_currency = models.CharField(max_length=3)
+    convertible_currency = models.CharField(max_length=3)
+    value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def validate_unique(self):
+        super().validate_unique(self)
+        now = datetime.now()
+
+        if CurrencyRate.objects.filter(
+            base_currency=self.base_currency,
+            convertible_currency=self.convertible_currency,
+            created_at__gte=datetime(now.year, now.month, now.day)
+        ).exists():
+            raise ValidationError(
+                'Latest currency rate {}-{} for period "{} {}" already exists'
+                .format(
+                    self.base_currency, self.convertible_currency,
+                    RATES_UPDATE_PERIOD_DAYS, 'Day'
+                )
+            )
