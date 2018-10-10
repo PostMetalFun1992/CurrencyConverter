d8ebd1bc023282f3df82b8117522f01cb44d1734
PostMetalFun1992 <kabanovkirill54@gmail.com>
2018-10-10 13:39:12 +0700

    Conversation stub view

--- app/converter/urls.py
+++ app/converter/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+from converter_app.views import CurrencyConversationView
+
+
 urlpatterns = [
+    path('conversation/', CurrencyConversationView.as_view()),
     path('admin/', admin.site.urls),
 ]
--- app/converter_app/views.py
+++ app/converter_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-# Create your views here.
+from converter_app.serializers import CurrencyConversationSerializer
+
+
+class CurrencyConversationView(generics.GenericAPIView):
+    serializer_class = CurrencyConversationSerializer
+
+    def put(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data)
